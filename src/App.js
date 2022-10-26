ad5b2465e0e8e2935267bc95672b54aeb95b4983
Xavski <xavioursaveski1@gmail.com>
2022-10-26 21:13:29 +1100

    edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Nav from "./components/Nav";
 import SearchPage from "./components/SearchPage";
 import DescriptionPage from "./components/DescriptionPage";
-import { useEffect, useState } from "react";
 
 
 function App() {
